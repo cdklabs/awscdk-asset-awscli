f90fad4f51b5a3f858584b5a611f18d01a6aec8f
Madeline Kusters <mkusters@amazon.com>
2022-09-06 20:12:54 -0700

    update name in projenrc

--- .projenrc.js
+++ .projenrc.js
@@ -3,8 +3,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Amazon Web Services, Inc.',
   cdkVersion: '2.0.0',
   defaultReleaseBranch: 'main',
-  name: 'awscdk-lambda-code-awscli-v1',
-  description: 'A Lambda Code object that contains the AWS CLI, for use in Lambda Layers',
+  name: 'awscdk-asset-awscli-v1',
+  description: 'An Asset construct that contains the AWS CLI, for use in Lambda Layers',
   repositoryUrl: 'git@github.com:cdklabs/awscdk-lambda-layer-awscli-v1.git',
 
   autoApproveOptions: {
