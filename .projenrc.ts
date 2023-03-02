57b7aa5cc89ae713a0c5949b5c50b3df4c965b54
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2023-03-02 11:39:17 -0500

    Update .projenrc.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -16,7 +16,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/cdklabs/awscdk-asset-awscli.git',
   homepage: 'https://github.com/cdklabs/awscdk-asset-awscli#readme',
   autoApproveOptions: {
-    allowedUsernames: ['aws-cdk-automation', 'dependabot[bot]'],
+    allowedUsernames: ['aws-cdk-automation', 'dependabot[bot]', 'mergify[bot]'],
     secret: 'GITHUB_TOKEN',
   },
   autoApproveUpgrades: true,
