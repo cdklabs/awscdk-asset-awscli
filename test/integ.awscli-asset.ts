c72d495095730cfdcaa062affd9e79be17ca4dea
Xia Zhao <78883180+xazhao@users.noreply.github.com>
2025-02-28 12:16:23 -0800

    chore: integration test failed because of a new feature flag set to true (#1148)
    This PR https://github.com/aws/aws-cdk/pull/33291 adding a new feature flag to update IAM policies. If the default value is false, it shouldn't have any changes.
    
    However, in the integration tests run by CodeBuild, it's enabled for some reasons, leading to a failed integration test.

--- test/integ.awscli-asset.ts
+++ test/integ.awscli-asset.ts
@@ -4,13 +4,18 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3_assets from 'aws-cdk-lib/aws-s3-assets';
 import * as cr from 'aws-cdk-lib/custom-resources';
+import { LAMBDA_CREATE_NEW_POLICIES_WITH_ADDTOROLEPOLICY } from 'aws-cdk-lib/cx-api';
 
 import { ASSET_FILE, LAYER_SOURCE_DIR } from '../lib';
 
 /**
  * Test verifies that AWS CLI is invoked successfully inside Lambda runtime.
  */
-const app = new cdk.App();
+const app = new cdk.App({
+  postCliContext: {
+    [LAMBDA_CREATE_NEW_POLICIES_WITH_ADDTOROLEPOLICY]: false,
+  },
+});
 
 const stack = new cdk.Stack(app, 'lambda-layer-awscli-integ-stack');
 const asset = new s3_assets.Asset(stack, 'layer-asset', {
