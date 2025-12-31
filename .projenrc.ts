37efee3e81fd68434d3df00a0549b01eabd67e30
kumvprat <kumvprat@amazon.de>
2025-12-31 13:23:01 +0100

    chore: turn off trusted publishing as latest version of publib now supports it

--- .projenrc.ts
+++ .projenrc.ts
@@ -52,7 +52,7 @@ const project = new CdklabsConstructLibrary({
   },
   publishToNuget: {
     // Trying to turn off trusted publishing default for NuGet package
-    trustedPublishing: "",
+    trustedPublishing: "false",
     dotNetNamespace: `Amazon.CDK.Asset.AwsCliV${MAJOR_VERSION}`,
     packageId: `Amazon.CDK.Asset.AwsCliV${MAJOR_VERSION}`,
   },
